81decfb4dacd396c35862b04570c30b98bad3bec
pltnv <anton.pletnew@mail.ru>
2023-06-22 22:51:10 +0300

    chore: updates i-input

--- src/components/IInput/IInput.vue
+++ src/components/IInput/IInput.vue
@@ -1,27 +1,34 @@
 <template>
-  <div class="i-input" :class="classes">
-    <input
-      v-model="localValue"
-      :maxlength="maxlength"
-      :autofocus="autofocus"
-      :disabled="disabled"
-      :required="required"
-      :placeholder="placeholder"
-      :readonly="readonly"
-      :type="localType"
-      @blur="handleBlur"
-      @input="updateValue($event.target.value)"
-    />
-    <div v-if="clearable && localValue.length && !disabled && !readonly">
-      <i class="mdi mdi-close" @click="clear" />
+  <div class="i-input">
+    <div class="i-input__label">
+      <div v-if="label" v-text="label" />
+      <sup v-if="required">*</sup>
     </div>
-    <div class="info">
-      <span v-if="count && amount > 0" v-text="amount" />
-      <span v-if="count && maxlength"> / </span>
-      <span v-if="maxlength" v-text="maxlength" />
-    </div>
-    <div v-if="isTypePassword && !disabled">
-      <i class="mdi" :class="passwordIcon" @click="toggleShowPassword" />
+    <div class="i-input__wrapper" :class="classes">
+      <input
+        :value="localValue"
+        :maxlength="maxlength"
+        :autofocus="autofocus"
+        :disabled="disabled"
+        :required="required"
+        :placeholder="placeholder"
+        :readonly="readonly"
+        :type="localType"
+        @blur="handleBlur"
+        @focus="handleFocus"
+        @input="updateValue($event.target.value)"
+      />
+      <div v-if="clearable && localValue.length && !disabled && !readonly">
+        <i class="mdi mdi-close" @click="clear" />
+      </div>
+      <div class="info">
+        <span v-if="count && amount > 0" v-text="amount" />
+        <span v-if="count && maxlength"> / </span>
+        <span v-if="maxlength" v-text="maxlength" />
+      </div>
+      <div v-if="isTypePassword && !disabled">
+        <i class="mdi" :class="passwordIcon" @click="toggleShowPassword" />
+      </div>
     </div>
   </div>
 </template>
@@ -127,6 +134,10 @@ export default {
       }
     };
 
+    let handleFocus = () => {
+      emit("focus");
+    };
+
     watch(
       () => props.modelValue,
       (newValue) => {
@@ -145,6 +156,7 @@ export default {
 
       updateValue,
       handleBlur,
+      handleFocus,
       clear,
       toggleShowPassword
     };
@@ -154,95 +166,99 @@ export default {
 
 <style lang="scss">
 .i-input {
-  display: flex;
-  align-items: center;
-  gap: 4px;
-  position: relative;
   width: 280px;
-  height: 30px;
-  padding: 6px;
-  border: 1px solid transparent;
-  border-radius: 10px;
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 1px 2px;
-
-  &.error {
-    border: 1px solid;
-    border-color: rgb(242, 48, 48);
-    box-shadow: none;
-  }
 
   &--block {
     width: 100%;
   }
 
-  &--outlined {
-    border: 1px solid gray;
-    box-shadow: none;
-
-    &:focus {
-      border: 1px solid black;
-    }
-
-    &:hover {
-      border: 1px solid black;
-    }
-
-    &.error {
-      border-color: rgb(242, 48, 48);
-    }
-  }
-
-  &--underline {
-    border-bottom: 1px solid gray;
-    border-radius: 0;
-    box-shadow: none;
-
-    :focus-within {
-      border-color: black;
-    }
-
-    &:hover {
-      border-color: black;
-    }
-
-    &.error {
-      border: none;
-      border-bottom: 1px solid rgb(242, 48, 48);
-    }
-  }
-
-  &--disabled {
-    opacity: 0.5;
-    pointer-events: none;
-  }
-
-  &--readonly {
-    pointer-events: none;
-  }
-
-  input {
-    width: 100%;
-    height: 100%;
-    outline: none;
-    border: none;
-    overflow: hidden;
-    white-space: nowrap;
-  }
-
-  .clear {
-    border: none;
-    background: none;
-    cursor: pointer;
-  }
-
-  .info {
+  &__wrapper {
     display: flex;
     align-items: center;
-    color: #676161;
-  }
+    gap: 4px;
+    position: relative;
+    width: 100%;
+    height: 30px;
+    padding: 6px;
+    border: 1px solid transparent;
+    border-radius: 10px;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 1px 2px;
 
-  i {
-    cursor: pointer;
+    &.error {
+      border: 1px solid;
+      border-color: rgb(242, 48, 48);
+      box-shadow: none;
+    }
+
+    &--outlined {
+      border: 1px solid gray;
+      box-shadow: none;
+
+      &:focus {
+        border: none;
+      }
+
+      &:hover {
+        border: 1px solid black;
+      }
+
+      &.error {
+        border-color: rgb(242, 48, 48);
+      }
+    }
+
+    &--underline {
+      border-bottom: 1px solid gray;
+      border-radius: 0;
+      box-shadow: none;
+
+      :focus-within {
+        border-color: black;
+      }
+
+      &:hover {
+        border-bottom: 1px solid black;
+      }
+
+      &.error {
+        border: none;
+        border-bottom: 1px solid rgb(242, 48, 48);
+      }
+    }
+
+    &--disabled {
+      opacity: 0.5;
+      pointer-events: none;
+    }
+
+    &--readonly {
+      pointer-events: none;
+    }
+
+    input {
+      width: 100%;
+      height: 100%;
+      outline: none;
+      border: none;
+      overflow: hidden;
+      white-space: nowrap;
+    }
+
+    .clear {
+      border: none;
+      background: none;
+      cursor: pointer;
+    }
+
+    .info {
+      display: flex;
+      align-items: center;
+      color: #676161;
+    }
+
+    i {
+      cursor: pointer;
+    }
   }
 }
 </style>
